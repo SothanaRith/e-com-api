b5b5991c6e5c72d8de1f5a8a70a7f50a75f0df7a
narithR <sothanarithheang@gmail.com>
2025-06-29 09:06:54 +0700

    set image

--- middleware/s3Upload.js
+++ middleware/s3Upload.js
@@ -6,6 +6,7 @@ const upload = multer({
     storage: multerS3({
         s3,
         bucket: process.env.AWS_BUCKET_NAME,
+        acl: 'public-read',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         key: (req, file, cb) => {
             cb(null, `products/${Date.now()}-${file.originalname}`);
